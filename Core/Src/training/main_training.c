b1bdb6c9649ab8782c7931f47493015e470dd27c
Dilraj Gill <dilgill1234@gmail.com>
2025-10-28 15:16:32 -0700

    updated for schedulers

--- Core/Src/training/main_training.c
+++ Core/Src/training/main_training.c
@@ -11,22 +11,39 @@
 
 #include "main.h"
 
-#include "gpio_training.h"
-
 #define LED_PORT GPIOC
 #define LED_PIN  13
+
+void my_task(void * pvParameters) {
+	configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
+
+	int count = 0;
+	for(;;) {
+		printMsg("Hello #%d\r\n", count++);
+	}
+}
 
 int main(void)
 {
 	setup_training();
-
 	int count = 0;
 
-	/* Infinite loop */
-	while (1)
-	{
-	  HAL_Delay(1000);
-	  printMsg("Hello #%d\r\n", count++);
+	BaseType_t xReturned;
+	TaskHandle_t xHandle = NULL;
+
+	xReturned = xTaskCreate(
+			my_task,
+			"LOL",
+			32,
+			(void *) 1,
+			tskIDLE_PRIORITY,
+			&xHandle
+	);
+
+	vTaskStartScheduler();
+
+	if(xReturned == pdPASS) {
+		vTaskDelete( xHandle );
 	}
 }
 
